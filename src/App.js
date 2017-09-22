64a80ceaf5184226b2494768c81d583ec189c160
Samuel Arno Saputra <samuelarnosaputra@gmail.com>
2017-09-22 17:18:22 +0700

    Fix backend_url App.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const url = `http://${process.env.REACT_APP_BACKEND}categories`;
+    const url = `http://${process.env.REACT_APP_BACKEND}/categories`;
     console.log('fetching from url', url);
-    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
+    this.setState({url: url})
+    fetch(url, { headers: { 'Authorization': 'Sam' },
                  credentials: 'include' } )
       .then( (res) => { return(res.text()) })
       .then((data) => {
@@ -32,6 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.url}
         <p>
           Talking to the backend yields these categories: <br/>
           {this.state.backend}
